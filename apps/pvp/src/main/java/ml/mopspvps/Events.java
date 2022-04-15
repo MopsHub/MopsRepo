e9a2e1e4266151fe4d52e344d5372ccdd411f24f
Bogdan Fediushin <bogdan.fediushin@Gmail.com>
2022-04-15 23:50:32 +0200

    как это поможет? яхз

--- apps/pvp/src/main/java/ml/mopspvps/Events.java
+++ apps/pvp/src/main/java/ml/mopspvps/Events.java
@@ -54,6 +54,7 @@ public class Events implements Listener {
 		this.cityEvents = Dependencies.getCityEvents();
 		this.logger = plugin.getLogger();
 	}
+
 	@EventHandler
 	public void onPlayerJoin(PlayerJoinEvent event) {
 		event.joinMessage(Component.empty());
