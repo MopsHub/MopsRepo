6daa9c420f447e1d70e81442f3cbac46170a06f4
Bogdan Fediushin <bogdan.fediushin@Gmail.com>
2022-04-16 11:25:09 +0200

    тут что-то будет

--- libs/mops-base/src/main/java/ml/mopsbase/MopsPlugin.java
+++ libs/mops-base/src/main/java/ml/mopsbase/MopsPlugin.java
@@ -1,12 +1,19 @@
 package ml.mopsbase;
 import ml.mopsbase.game.GameSession;
 import ml.mopsbase.game.GamePlayer;
+import ml.mopsutils.Folder;
 import ml.mopsutils.Resources;
 import org.bukkit.plugin.java.JavaPlugin;
+import com.fasterxml.jackson.core.
 
 import java.util.List;
 
 public class MopsPlugin extends JavaPlugin {
 	private List<GameSession> gameSessions;
+
+	private Config loadConfig(String path, Folder f) {
+		return null
+	}
+
 
 }
