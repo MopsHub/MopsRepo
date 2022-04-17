0e3f8462ca874e92a296eee244ad4e9185f7801f
Bogdan Fediushin <bogdan.fediushin@Gmail.com>
2022-04-17 13:28:36 +0200

    летсгоу?

--- apps/mopspvps/src/main/java/ml/mopspvps/Plugin.java
+++ apps/mopspvps/src/main/java/ml/mopspvps/Plugin.java
@@ -1,9 +1,8 @@
 package ml.mopspvps;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
-import ml.mopsbase.Config;
 import ml.mopsbase.MopsPlugin;
+import ml.mopsexception.MopsConfigException;
+import ml.mopsexception.configs.ParseCfgToYAMLException;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.format.NamedTextColor;
@@ -14,10 +13,8 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
-import java.io.File;
 import java.net.http.WebSocket.Listener;
 import java.sql.Timestamp;
-import java.util.Arrays;
 import java.util.logging.Logger;
 
 /**
@@ -30,6 +27,7 @@ import java.util.logging.Logger;
 public class Plugin extends MopsPlugin implements Listener, CommandExecutor {
 	static Dependencies dependencies = null;
 	static Events events = null;
+	static Logger logger;
 
 	final TextComponent restartMessage = Component.text("Сервер был перезагружен!").color(NamedTextColor.GREEN);
 
@@ -39,94 +37,16 @@ public class Plugin extends MopsPlugin implements Listener, CommandExecutor {
 		Timestamp enableTimeStamp = new Timestamp(System.currentTimeMillis());
 		Bukkit.broadcast(restartMessage);
 
-//		File cfgFile = new File(getDataFolder().getAbsolutePath() + "/config.yml");
-//		String cfgText = "";
-//
-//		try {
-//			Scanner myReader = new Scanner(cfgFile);
-//			while (myReader.hasNextLine()) {
-//				String data = myReader.nextLine();
-//				cfgText = cfgText + data;
-//			}
-//			myReader.close();
-//		} catch (FileNotFoundException e) {
-//			logger.info("Custom config not found. Loading default one");
-//			try {
-//				Scanner myReader = new Scanner(cfgFile);
-//				while (myReader.hasNextLine()) {
-//					String data = myReader.nextLine();
-//					cfgText = cfgText + data;
-//				}
-//				myReader.close();
-//			}
-//			catch (Exception ex) {
-//				logger.warning("default config not found.");
-//				logger.warning(Arrays.toString(ex.getStackTrace()));
-//			}
-//		}
-//
-//		if (cfgText.isBlank()) {
-//			logger.warning("Both default and custom configs are not found/empty/blank");
-//			this.setEnabled(false);
-//			return;
-//		}
-
-		File cfgFile = new File(getDataFolder().getAbsolutePath() + "/config.yml");
-		boolean isCustomConfigOk = false;
-		boolean isDefaultConfigOk;
-		ObjectMapper objectMapper = new ObjectMapper(new YAMLFactory());
-
 		try {
-			if (cfgFile.exists()) {
-				logger.info("Trying to parse config at path: " + cfgFile.getAbsolutePath());
-				this.config = objectMapper.readValue(cfgFile, Config.class);
-				try {
-					logger.info("Custom config: \n" + this.config.parseToString());
-					isCustomConfigOk = true;
-					if (this.config.parseToString().isBlank()) {
-						isCustomConfigOk = false;
-						logger.info("Custom config is empty.");
-					}
-				} catch (Exception e) {
-					logger.warning("Unable to turn config into string due to:\n" + e.getMessage() + "\n" + Arrays.toString(e.getStackTrace()));
-					isCustomConfigOk = false;
-				}
-			}
-		} catch (Exception e) {
-			logger.warning("Exception while parsing custom config: " + e.getMessage() + "\n " + Arrays.toString(e.getStackTrace()));
+			loadConfig(logger);
+		} catch (MopsConfigException e) {
+			logger.warning(e.getMessage());
 		}
-
 		try {
-			logger.info("Trying to parse default config");
-
-			defaultConfig = objectMapper.readValue(this.getResource("config.yml"), Config.class);
-			try {
-				logger.info("Default config: \n" + this.defaultConfig.parseToString());
-				isDefaultConfigOk = true;
-				if (this.defaultConfig.parseToString().isBlank()) {
-					isDefaultConfigOk = false;
-					logger.warning("Default config is empty");
-				}
-				} catch (Exception e) {
-					logger.warning("Unable to turn config into string due to:\n" + e.getMessage() + "\n" + Arrays.toString(e.getStackTrace()));
-					isDefaultConfigOk = false;
-				}
-			} catch (Exception e) {
-			logger.warning("Exception while parsing default config: " + e.getMessage() + "\n " + Arrays.toString(e.getStackTrace()));
-			this.setEnabled(false);
-			return;
+			logConfig(logger);
+		} catch (ParseCfgToYAMLException e) {
+			logger.warning(String.valueOf(e.getStackTrace()));
 		}
-
-		if (!isCustomConfigOk) {
-			if (isDefaultConfigOk) {
-				this.config = this.defaultConfig;
-			} else {
-				logger.warning("Both custom and default configs are blank/corrupted/not working/not found");
-				this.setEnabled(false);
-				return;
-			}
-		}
-
 
 		dependencies = new Dependencies(Plugin.this);
 
@@ -192,5 +112,7 @@ public class Plugin extends MopsPlugin implements Listener, CommandExecutor {
 	static Dependencies getDependencies() {
 		return dependencies;
 	}
+
+
 }
 
--- libs/mopsbase/src/main/java/ml/mopsexception/configs/ParseCfgToYAMLException.java
+++ libs/mopsbase/src/main/java/ml/mopsexception/configs/ParseCfgToYAMLException.java
@@ -4,8 +4,8 @@ import ml.mopsexception.MopsConfigException;
 
 import java.util.Arrays;
 
-public class ParsingConfigToYAMLStringException extends MopsConfigException {
-	public ParsingConfigToYAMLStringException(Exception e) {
+public class ParseCfgToYAMLException extends MopsConfigException {
+	public ParseCfgToYAMLException(Exception e) {
 		super("Error while parsing Config into yaml string\nException:" + e.getLocalizedMessage() + "\nStacktrace:\n\n\n" + Arrays.toString(e.getStackTrace()));
 	}
 }
