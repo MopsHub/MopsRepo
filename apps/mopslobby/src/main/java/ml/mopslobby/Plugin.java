4b89c7a7a7316aa5a91035c65a7fa6a26c637dc9
SirCat07 <quant08@outlook.com>
2022-04-18 13:22:41 +0300

    ~~ОЧЕНЬ ПОЛЕЗНЫЕ~~ фичи в лобби плагине

--- apps/mopslobby/src/main/java/ml/mopslobby/Plugin.java
+++ apps/mopslobby/src/main/java/ml/mopslobby/Plugin.java
@@ -1,9 +1,42 @@
 package ml.mopslobby;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandExecutor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Plugin extends JavaPlugin implements Listener, CommandExecutor {
+    @Override
+    public void onEnable() {
+        Bukkit.getServer().getPluginManager().registerEvents(this, this);
+    }
 
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent event) {
+        Player player = event.getPlayer();
+        if(player.getScoreboardTags().contains("admin")) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onBlockPlace(BlockPlaceEvent event) {
+        Player player = event.getPlayer();
+        if(player.getScoreboardTags().contains("admin")) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onInteract(PlayerInteractEvent event) {
+        Player player = event.getPlayer();
+        if(player.getScoreboardTags().contains("admin")) {
+            event.setCancelled(true);
+        }
+    }
 }
